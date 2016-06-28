6a7082a8ab24731b57f1ae312a5cb2e93252f138
gautampoornachandra <gautam.poornachandra@gmail.com>
2016-06-27 19:06:05 -0500

    Lab Login Page

---
+++ src/com/servlets/LoginServlet.java
@@ -0,0 +1,62 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		String id = request.getParameter("name");
+		String pass = request.getParameter("password");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		RequestDispatcher rd = null;
+		request.setAttribute(id, "name");
+		if (id.equals("admin") && pass.equals("admin")) {
+			
+	        out.println("<h2>" +"Welcome "+ id + "</h2>");
+			
+		} else {
+			
+			out.println("<h2>" +"Invalid username or Password!"+ "</h2>");
+			
+		}
+		out.close();
+		
+	}
+
+}
